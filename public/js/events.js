12e49673b2665f39b81e0c0a28996fbffc214ddd
Sonali singh <sonali122000@gmail.com>
2020-11-10 13:36:12 +0530

    chnange heading styles

--- public/js/events.js
+++ public/js/events.js
@@ -72,13 +72,4 @@ accordionItemQuestions.forEach((accordionItemQues) => {
   });
 });
 
-function textCounter(field,field2,maxlimit)
-{
- var countfield = document.getElementById(field2);
- if ( field.value.length > maxlimit ) {
-  field.value = field.value.substring( 0, maxlimit );
-  return false;
- } else {
-  countfield.value = maxlimit - field.value.length;
- }
-}
+
